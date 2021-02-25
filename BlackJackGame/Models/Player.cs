d359419719b5758ea129e829ce9475329951254e
Philip Rasmusson <rasmussonphilip@gmail.com>
2021-02-25 15:41:54 +0100

    Created method CreatePlayer in Player class

--- BlackJackGame/Models/Player.cs
+++ BlackJackGame/Models/Player.cs
@@ -1,15 +1,30 @@
-﻿namespace BlackJackGame
+﻿using System.Collections.Generic;
+using System;
+
+namespace BlackJackGame
 {
     public class Player
     {
-        public int cards { get; set; }
-        public int score { get; set; }
-        public int playerName { get; set; }
-        public bool stay { get; set; } = false;
+        public int Bet { get; set; } = 0;
+        public List<string> Cards { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; } = 0;
+        public bool Stay { get; set; } = false;
 
-        public void CreatePlayer()
+        public Player(int playerNumber)
+        {
+            Name = "Player " + playerNumber;
+        }
+
+        public static void CreatePlayer(int numberOfPlayers)
         {
 
+            var playersList = new List<Player>();
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                playersList.Add(new Player(i + 1));
+            }
         }
 
     }
