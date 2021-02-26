852486f9979741069e523c49d675bc4a8238d15c
Mohammad-khk <mu92.sm11@gmail.com>
2021-02-26 15:11:19 +0100

    Created Unit test for Deck class

--- BlackJackGame/Controllers/Deck.cs
+++ BlackJackGame/Controllers/Deck.cs
@@ -6,12 +6,12 @@ using System.Text;
 
 namespace BlackJackGame
 {
-    public class Deck
+    public static class Deck
     {
         //Card GetCards = new Card();
 
-        public string[] cards { get; set; } = new string[13] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "J", "K" };
-        public string[] cardType { get; set; } = new string[4] { "♥", "♣", "♠", "♦" };
+        public static string[] cards { get; set; } = new string[13] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "J", "K" };
+        public static string[] cardType { get; set; } = new string[4] { "♥", "♣", "♠", "♦" };
 
         /*----Tänkte att skapa list of Card klassen för att underlätta åtgången på Card Number----*/
         /*public List<Card> MyCards { get; set; } = new List<Card>
@@ -21,7 +21,7 @@ namespace BlackJackGame
         /// Generates the deck(52 Cards).
         /// </summary>
         /// <returns></returns>
-        public List<string> GenerateDeck()
+        public static List<string> GenerateDeck()
         {
             List<string> listOfCards = new List<string>();
             for (int i = 0; i < cardType.Length; i++)
@@ -39,18 +39,34 @@ namespace BlackJackGame
         /// </summary>
         /// <param name="deck">The deck.</param>
         /// <returns></returns>
-        public List<string> BuildDeckForGame(List<string> deck)
+        public static List<string> BuildDeckForGame(List<string> deck)
         {
-            List<string> randomCards = new List<string>();
+            List<string> gameDeck = new List<string>();
             for (int i = 0; i < 4; i++)
             {
-                var random = new Random();
-                var randomList = deck.OrderBy(i => random.Next(104, 156));
-                foreach (var item in randomList)
+                foreach (var item in deck)
                 {
-                    randomCards.Add(item);
+                    gameDeck.Add(item);
                 }
             }
+            return gameDeck;
+        }
+        /// <summary>
+        /// Suffles the list.
+        /// </summary>
+        /// <param name="deck">The deck.</param>
+        /// <returns></returns>
+        public static List<string> SuffleList(List<string> deck)
+        {
+            List<string> randomCards = new List<string>();
+
+            var random = new Random();
+            var randomList = deck.OrderBy(i => random.Next(0,208));
+            foreach (var item in randomList)
+            {
+                randomCards.Add(item);
+            }
+
             return randomCards;
         }
 
@@ -59,8 +75,7 @@ namespace BlackJackGame
         /// </summary>
         /// <param name="deck">The deck.</param>
         /// <returns></returns>
-        public string GetCard(List<string> deck)
-        
+        public static string GetCard(List<string> deck)
         {
             var random = new Random();
             var randomList = deck.OrderBy(i => random.Next(104, 156));
@@ -73,10 +88,12 @@ namespace BlackJackGame
 
             return card;
         }
-
-        public void ResetCards()
+        /// <summary>
+        /// Resets the cards .
+        /// </summary>
+        public static void ResetCards()
         {
-            throw new System.NotImplementedException();
+            Deck.SuffleList(BuildDeckForGame(GenerateDeck()));
         }
     }
 }
