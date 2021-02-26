613d055f6bf3e9ceffeb2417a1cf231e3adccf05
Zia Alekozai <88ziaale@gafe.molndal.se>
2021-02-26 10:40:27 +0100

    Gjord test på lite metoder

--- BlackJackGame/Models/BlackJack.cs
+++ BlackJackGame/Models/BlackJack.cs
@@ -1,35 +1,56 @@
-﻿namespace BlackJackGame
-{
-    public class BlackJack 
+﻿using System;
+namespace BlackJackGame
     {
-        public void CreatePlayer()
-        {
-            throw new NotImplementedException();
-        }
 
-        public void GiveCard()
+    public static class BlackJack
         {
+        public static void CreatePlayer()
+            {
             throw new NotImplementedException();
-        }
+            }
 
-        public void ResetDeck()
-        {
+        public static void GiveCard()
+            {
             throw new NotImplementedException();
-        }
+            }
 
-        public void RunGame()
-        {
+        public static void ResetDeck()
+            {
             throw new NotImplementedException();
-        }
+            }
 
-        public int SeclectNumberOfPlayers()
-        {
-            throw new NotImplementedException();
-        }
+        public static void RunGame()
+            {
+            
+            }
 
-        public int SelectTable()
-        {
-            throw new NotImplementedException();
+        public static int SeclectPlayers(int nummber)
+            {
+            if (nummber < 1)
+                {
+                nummber = 1;
+                Console.WriteLine("Nummber is too low. change it to 1 player");
+                }
+            if (nummber > 7)
+                {
+                nummber = 7;
+                }
+
+            return nummber;
+
+            }
+
+        public static int SelectTable(int table)
+            {
+            if (table <1)
+                {
+                table = 1;
+                }
+           else if (table > 4)
+                {
+                table = 4;
+                }
+            return table;
+            }
         }
     }
-}
