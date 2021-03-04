38faa199eabc58402c6036e84d8d9b5b96bed552
Mohammad-khk <mu92.sm11@gmail.com>
2021-03-04 03:15:51 +0100

    Created card class

--- BlackJackGame/Models/Card.cs
+++ BlackJackGame/Models/Card.cs
@@ -8,8 +8,62 @@ namespace BlackJackGame.Models
 {
     public class Card
     {
-        public string Number { get; set; }
-        public string Type { get; set; }
+        public enum ECardSymbol
+        {
+            Club = '♥',
+            Diamond = '♣',
+            Heart = '♠',
+            Spades = '♦',
+        }
+        public enum ECardNumber
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13,
+        }
+
+        public string CardSymbol { get; set; }
+        public int CardNumber { get; set; }
+
+        public Card(int CardNumber, string CardSymbol)
+        {
+            this.CardNumber = CardNumber;
+            this.CardSymbol = CardSymbol;
+        }
+
 
     }
+    public class TestDecK
+    {
+        private static int[] cards { get; set; } = new int[13] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+        private static string[] cardType { get; set; } = new string[4] { "♥", "♣", "♠", "♦" };
+        public static List<Card> GetDeck()
+        {
+            List<Card> gameDeck = new List<Card>();
+
+            for (int i = 0; i < cardType.Length; i++)
+            {
+                for (int j = 0; j < cards.Length; j++)
+                {
+                    gameDeck.Add(new Card(cards[j], cardType[i]));
+                }
+            }
+
+            return gameDeck;
+        }
+    }
+
+
+
+
 }
